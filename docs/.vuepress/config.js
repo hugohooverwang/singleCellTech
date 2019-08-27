4ac1818b3c0c3180616fcba552e3e240984abae7
hugohooverwang <hugohooverwang@126.com>
2019-08-27 11:28:37 +0800

    modify config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,18 @@
 module.exports = {
   title: 'Single Cell Technology',
   description: 'technical notes',
-  base: '/singlecelltech/'
+  base: '/singlecelltech/',
+  themeConfig: {
+    nav: [
+      { text: 'Concepts', link: 'concepts' },
+      {
+        text: 'Methods',
+        items: [
+          { text: 'Isolation', link: 'isolation' },
+          { text: 'Data Analysis', link: 'data' }
+        ]
+      },
+      { text: 'Clinical Applications', link: 'publications'}
+    ]
+  }
 }
